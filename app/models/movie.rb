da54fcb02f9c4f7e1ec2a2f513274c3cdec4e3b7
Kyler Best <kyler.b@live.com>
2022-03-18 21:58:09 -0500

    commit tree 3773677c9fdb3188e09219477f0277c6c950cc04

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,25 @@
 class Movie < ActiveRecord::Base
-
+    def self.create_with_title title
+        new = self.new
+        new.title = title
+        new.save
+    end
+    def self.first_movie
+        self.first
+    end
+    def self.last_movie
+        self.last
+    end
+    def self.movie_count
+        self.count
+    end
+    def self.find_movie_with_id id
+        self.find(id)
+    end
+    def self.find_movie_with_attributes attributes
+        self.find_by(attributes)
+    end
+    def self.find_movies_after_2002
+        self.where(:release_date > 2002)
+    end
 end
